dbbd50e20ccf3663242d3c1c5f1f0770079e8cb9
Joao Santana <jfsant2017@gmail.com>
2020-05-27 21:57:31 -0300

    Completed patch

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/operators'; // import { map } from 'rxjs/add/operator/map';
+// import { map } from 'rxjs/operators'; // import { map } from 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -9,8 +10,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number){
-    return this._http.get(this._albumUrl).pipe(
-      map ((res: Response) => res.json())
-    );
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
